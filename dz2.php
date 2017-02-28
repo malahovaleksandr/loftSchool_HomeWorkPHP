8a2122d88bcf3753cfc7e2f15e5f93af70f46936
“malahovaleksandr” <pro100boy@gmail.com>
2017-02-28 17:29:08 +0200

    второе ДЗ сделал до 6 задания

--- dz2.php
+++ dz2.php
@@ -1,7 +1,7 @@
 <?php
 $example='задание ';
 //1
-echo $example.'1<br>';
+echo $example.'1<br><br>';
 
 function arrayEcho($arr,$true=0){
 
@@ -22,7 +22,7 @@ arrayEcho($examp);
 echo arrayEcho($examp,8);
 
 //2
-echo $example.'2<br>';
+echo '<br>'.$example.'2<br><br>';
 
 function arifmetic ($val,$ar){
     switch ($ar) {
@@ -62,7 +62,7 @@ echo arifmetic($numders,'+').'<br>';
 echo arifmetic($numders,'-').'<br>';
 
 //3 не доконца!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-echo $example.'3<br>';
+echo '<br>'.$example.'3<br><br>';
 function easyCalc ($ar,$val1,$val2,$val3){
     switch ($ar) {
         case '+':
@@ -95,4 +95,48 @@ easyCalc('*',8,9,2.5);
 easyCalc('/',8,9,2.5);
 
 //4 ---------------------------------------------
-echo $example.'4<br>';
+echo '<br>'.$example.'4<br><br>';
+function tableArifmetic($valu1,$value2){
+    if(is_int($valu1) && is_int($value2) ){
+        echo '<table>';
+        for($i=1;$i<=$valu1;$i++){
+            echo '<tr> ';
+            for($e=1;$e<=$value2;$e++) {
+                echo '<td style="padding:5px;"> '.$i.' * '.$e.' </td>';
+            }
+            echo ' </tr>';
+        }
+        echo '</table>';
+    } else {
+        echo 'одной или оба числа не целые';
+    }
+
+}
+tableArifmetic(6,7);
+
+//5 ---------------------------------------------
+echo '<br>'.$example.'5<br><br>';
+
+echo 'функция 1<br>';
+function revers($word){
+
+    if(strrev(trim($word))==trim($word)){
+        $answer= 'true';
+    }else {
+        $answer= 'false';
+    }
+    return $answer;
+}
+echo revers('tetetet').'<br>';
+echo revers('tetetsaaset').'<br>';
+
+echo '<br>функция 2<br>';
+
+function inspection($arg){
+    if($arg=='true'){
+        echo 'правда';
+    } elseif ($arg=='false'){
+        echo 'ложь';
+    }
+}
+inspection(revers('tetetet'));
