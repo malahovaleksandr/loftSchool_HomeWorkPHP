aadf2581980e330e9b811db94929af85057a2cdd
“malahovaleksandr” <pro100boy@gmail.com>
2017-04-02 14:52:45 +0300

    сделано задание №4Ð

--- 4/index.php
+++ 4/index.php
@@ -50,27 +50,39 @@ fwrite($fp, $text);
 
 $xml = simplexml_load_file('data.xml');
 echo '<pre>';
-//print_r($xml);
-//print_r($xml['Type']);
-foreach ($xml as $info) {
-    if($info['Type']){
-        echo '<b>'.$info['PartNumber'].'</b><br>';
-        echo ' Имя '.$info->Name.'<br>  ';
-        echo ' улица '.$info->Street.'<br> ';
-        echo ' город '.$info->City.'<br> ';
-        echo ' штат '.$info->State.'<br> ';
-        echo ' индекс '.$info->Zip.'<br> ';
-        echo ' страна '.$info->Country.'<br> ';
-    }
-    if($info['PartNumber']){
-        echo '<b>'.$info['item'].'</b><br>';
-        echo ' название продукта '.$info->ProductName.'<br>  ';
-        echo ' качество '.$info->Quantity.'<br> ';
-        echo ' цена '.$info->USPrice.'<br> ';
-        echo ' дата доставки  '.$info->Comment.'<br> ';
-    }
 
+//print_r($xml);
+
+echo 'номер заказа '.$xml['PurchaseOrderNumber'].'<br>';
+echo 'дата заказа '.$xml['OrderDate'].'<br>';
+
+foreach ($xml as $info) {
+     if($info['Type']){
+            echo '<br><b>'.$info['Type'].'</b><br>';
+            echo '<b>'.$info['PartNumber'].'</b><br>';
+            echo ' Имя '.$info->Name.'<br>  ';
+            echo ' улица '.$info->Street.'<br> ';
+            echo ' город '.$info->City.'<br> ';
+            echo ' штат '.$info->State.'<br> ';
+            echo ' индекс '.$info->Zip.'<br> ';
+            echo ' страна '.$info->Country.'<br> ';
+        }
 }
+echo '<br>пометка по заказу '.$xml->DeliveryNotes.'<br<br>';
+$array2=$xml->Items->Item;
+
+foreach($array2 as $value=>$key)
+{
+    echo '<b>номер сборки '.$key['PartNumber'].'</b>';
+    echo '<b>'.$key['item'].'</b><br>';
+    echo ' название продукта '.$key->ProductName.'<br>  ';
+    echo ' качество '.$key->Quantity.'<br> ';
+    echo ' цена '.$key->USPrice.'<br> ';
+    echo ' дата доставки  '.$key->Comment.'<br><br> ';
+}
+
+
+
 
 //              НЕ ЗАКОНЧИЛ ПЕРВЫЙ ПУНКТ------------------------------------------
 $file = file('data.xml');
@@ -106,24 +118,37 @@ fwrite($jsf, $dec);
          if($i == 2){
              $json[$i]='change';
          }
-
          $newArray[]=$json[$i];
      }
 
      echo 'внесли изменения<br>';
-     //print_r($newArray);
 
      $dec = json_encode($newArray);
-
      $jsf = fopen("output2.json", "w");
 
     // записываем в файл текст
      fwrite($jsf, $dec);
  }
+//функция для проверка на отличия
+function findDifferent($file1,$file2){
+    $fp1 = file_get_contents($file1);
+    $fp2 = file_get_contents($file2);
+    $json1 = json_decode($fp1,true);
+    $json2 = json_decode($fp2,true);
+
+    for($i=0;$i < count($json1);++$i){
+       if($json1[$i]!=$json2[$i])
+       {
+           echo 'отличается '.$json1[$i].' от значения '.$json2[$i];
+       }
+    }
+}
+
 
 $rand=mt_rand(0,30);
 if($rand%2 == 0){
     changeJson("output.json");
+    findDifferent('output.json','output2.json');
 } else {
     echo 'Ни чего неменяем';
 }
@@ -156,13 +181,13 @@ echo '<br>'.$example.'4<br><br>';
 $curl='https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json';
 
 $ch = curl_init($curl);
-// Запускаем
-$answer = curl_exec($ch);
+//// Запускаем
+//$answer = curl_exec($ch);
 
  //Проверяем наличие ошибок
-if (!$answer) {
-    $info = curl_getinfo($ch);
-    echo 'Прошло ', $info['total_time'], ' секунд во время запроса к ', $info['url'], "\n";
-}
+//if (!$answer) {
+//    $info = curl_getinfo($ch);
+//    echo 'Прошло ', $info['total_time'], ' секунд во время запроса к ', $info['url'], "\n";
+//}
 echo '<pre>';
-echo $answer;
+echo 'ответ '.$answer;
